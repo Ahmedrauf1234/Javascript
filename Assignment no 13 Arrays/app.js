b9498b37acdf75058721357d1f3010a7d3fce1ca
Ahmedrauf1234 <Ahmedrauf261@gmail.com>
2023-11-09 17:05:29 +0500

    practice done

--- Assignment no 13 Arrays/app.js
+++ Assignment no 13 Arrays/app.js
@@ -12,22 +12,22 @@ console.log(fruits[7][2][0]);
 
 //write print pakistan in console
 let phal = ["apple","orange",["Ahmed","talha",45,false,"abc"],"banana","mango",true,45,["nashpati","computer",["india","america",["pakistan"]],]]
-console.log(phal[7][2][2][0]);
+console.log(phal[7][2][2][0]); //pakistan
 
 // writeb print india console
 
 let abc = ["apple",["maldives",["India"],"duai"],"orange",["Ahmed","talha",45,false,"abc"],"banana","mango",true,45,["nashpati","computer",["india","america",["pakistan"]],]]
-console.log(abc[1][1][0]);
+console.log(abc[1][1][0]); //india
 
 // write print hello console
 
 let hello = ["apple",["maldives",["India"],"duai"],"orange",["Ahmed","talha",45,false,"abc"],"banana","mango",true,45,["nashpati","computer",["india","america",["pakistan","australia",["hello?"]  ]  ]  ]  ]
-console.log(hello[8][2][2][2][0]);
+console.log(hello[8][2][2][2][0]); //hello
 
 // write print javascript in console
 
-let hello1 = ["apple",["maldives",["India"],"duai"],"orange",["Ahmed","talha",45,false,"abc"],"banana","mango",true,45,["nashpati","computer",["india","america",["pakistan",["Html","css",["javascript"]],"australia",["hello?"]  ]  ]  ]  ]
-console.log(hello1[8][2][2][1][2][0])
+let hello1 = ["apple",["maldives",["India"],"dubai"],"orange",["Ahmed","talha",45,false,"abc"],"banana","mango",true,45,["nashpati","computer",["india","america",["pakistan",["Html","css",["javascript"]],"australia",["hello?"]  ]  ]  ]  ]
+console.log(hello1[8][2][2][1][2][0]) //javascript
 // console.log(hello1[8][2][2][3][0]); ////hello? print
 
 // change arrays in ressign values && conditions
@@ -85,7 +85,7 @@ console.log(arr2);
 let arr3 = arr2.slice(1 , 4) 
 console.log(arr3);
 
-let abc1 = arr2.slice(0)
+let abc1 = arr2.slice(0) //0 se leke end tk pora print hoga
 console.log(abc1);
 
 
@@ -121,16 +121,46 @@ const pak = ["karachi","lahore","peshawar","quetta"]
 const reversepak = pak.reverse()
 console.log(reversepak);
 
-// indexof jo bhi aap naam doge agar wo arr me mojood hoga uska index number batadega
+// indexof jo bhi aap naam doge agar wo arr me mojood hoga uska index number batadega agar majood nh hoga wohh to -1 ajayega means mojood nhi
  const index = ["Ahmed","banana","talha","hammad","cat"]
  const newindex = index.indexOf("hammad") //3
  console.log(newindex);
 
  //last indexOf
 
-const a = ["Ahmed","banana","talha","hammad","cat"]
-const newA = a.lastIndexOf("cat")
+const a = ["Ahmed","banana","talha","hammad","cat","abc"]
+const newA = a.lastIndexOf("abc")
 console.log(newA);
+
+//find last value of index number in array means abdullah index num? 
+let Val  = ["Ahmed","axhad","hassan","abdullah"]
+let Val1 = Val.at(-1)
+console.log(Val1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
